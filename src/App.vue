acdac42a6ba099493fc8767509b38d012b700616
Bharadwaj <bharadwajm@gmail.com>
2025-12-31 04:21:31 -0800

    Updated header

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <h1>Welcome to ASI world :) </h1>
-	<h2>Upload your data to sget Gen Ai predictions</h2>
+	<h2>BM to  your data to sget Gen Ai predictions</h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
